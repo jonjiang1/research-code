b1a09f7c1259dcf50f3514264cee7c780bfe2e13
Eddy Chiao <eddychiao@lawn-128-61-118-138.lawn.gatech.edu>
2018-10-04 14:42:16 -0400

    added new main.py files

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+THIS IS THE MAIN FILE
+- All operation and commands will be issued from here
+- Methods are defined in ops.py (please refer)
+
+- Authors: Edward Chiao and Jon Jiang (in the lab of Lelia Glass, Ph. D)
+
+
+"""
